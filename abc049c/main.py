1dd998eff92c058b8d0450d848220aa393ed6adb
bun913 <imanau03@gmail.com>
2022-05-06 07:42:26 +0900

    feat: abc049cを追加

--- abc049c/main.py
+++ abc049c/main.py
@@ -19,3 +19,38 @@ l = list(map(int, input().split()))
 https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
 
+S = input()
+s = S[::-1]
+org_s = S[::-1]
+
+is_finish = False
+t = ""
+
+
+while is_finish is False:
+    candidates = [
+        "dream"[::-1],
+        "dreamer"[::-1],
+        "erase"[::-1],
+        "eraser"[::-1]
+    ]
+    # 逆さまにして候補文字が挿入できそうなら行って残りの文字を次のループで判定する
+    if s.startswith(candidates[0]):
+        t += candidates[0]
+        s = s.replace(candidates[0], "", 1)
+    elif s.startswith(candidates[1]):
+        t += candidates[1]
+        s = s.replace(candidates[1], "", 1)
+    elif s.startswith(candidates[2]):
+        t += candidates[2]
+        s = s.replace(candidates[2], "", 1)
+    elif s.startswith(candidates[3]):
+        t += candidates[3]
+        s = s.replace(candidates[3], "", 1)
+    else:
+        is_finish = True
+
+if org_s == t:
+    print("YES")
+else:
+    print("NO")
